7be77fcd747cda2a1349882ef4a854b3d48b3bc4
Yi Chiang and russ vetsper <yi+russ@>
2016-06-21 14:54:07 -0700

    add Palindromes

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$ (document).ready(function(){
+
+$("form#input").submit(function(event){
+  event.preventDefault();
+var originalString=$("#wework").val().toLowerCase();//"Woo" ====>"woo"
+var b=originalString.split("");// ["W","O","O"]
+var c=b.reverse();
+var d=c.join("");
+console.log(c.join(""));
+if(originalString===d){
+  console.log("we are okay");
+}else{
+  console.log("it is not okay");
+}
+});
+
+
+
+});
